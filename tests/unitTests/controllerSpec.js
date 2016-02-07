dcbd876882fdf099f2499b5f1607bb6e75a34f27
Vladyslav <Vladyslav_Istomin@epam.com>
2016-02-07 22:31:10 +0200

    Start writing tests

---
+++ tests/unitTests/controllerSpec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* jasmine specs for controllers go here */
+describe('PhoneCat controllers', function() {
+
+	beforeEach(module('app'));
+
+	describe('PhoneListCtrl', function(){
+
+	});
+});
